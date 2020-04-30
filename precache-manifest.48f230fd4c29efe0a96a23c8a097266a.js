7ff4943833a31b6bb8ac1c1279baf51ecee017a3
Rishav Jaiswal <rishavjaiss14@gmail.com>
2020-04-30 18:23:24 +0530

    Updates

---
+++ precache-manifest.48f230fd4c29efe0a96a23c8a097266a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "856d186980f65584faf89a7692ee3976",
+    "url": "/financial-portfolio-tracker-app/index.html"
+  },
+  {
+    "revision": "09e560694f95bc0585b5",
+    "url": "/financial-portfolio-tracker-app/static/css/main.e33969d8.chunk.css"
+  },
+  {
+    "revision": "d27ae17421cac187315b",
+    "url": "/financial-portfolio-tracker-app/static/js/2.ec7e760f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/financial-portfolio-tracker-app/static/js/2.ec7e760f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09e560694f95bc0585b5",
+    "url": "/financial-portfolio-tracker-app/static/js/main.1f89a3e5.chunk.js"
+  },
+  {
+    "revision": "4b8bfb854203ac7ab457",
+    "url": "/financial-portfolio-tracker-app/static/js/runtime-main.b0bf98b3.js"
+  }
+]);

fb84bbbb44d9d4fb2a110b8c9cdb322acf38278f
Rishav Jaiswal <rishavjaiss14@gmail.com>
2020-06-05 01:51:29 +0530

    Update env.js

--- src/env.js
+++ src/env.js
@@ -1,5 +1,4 @@
-//export const API_KEY = "K4C1OCSP489EZS33";
-export const API_KEY = "5CF1O9FBXRKM2TA7";
+export const API_KEY = "YOUR_API_KEY";
 export const stocksUrl =
   "https://financial-portfolio-trac-73f3e.firebaseio.com/addStocks.json";
 
